a5c0dfc2ab4837c75cef0fcfde60ff8d4a116138
jninan13 <ninanjaden@gmail.com>
2024-10-29 13:29:41 -0500

    File strucutre altered to have folders for each view and fix image errors that were occurring.

--- panda-pos/src/app/CustomerView/page.js
+++ panda-pos/src/app/CustomerView/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from 'react';
 import styles from './CustomerViewStyle.module.css'; // Import CSS module
 import MainMenuComponent from './CustomerComponents/MainMenuComponent';
