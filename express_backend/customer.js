2d66187f79ea49da5a4e9c056020d02cdc72bdb6
Daniel Fuhrmann <142857162+FuhrmannDaniel@users.noreply.github.com>
2024-11-13 10:59:27 -0600

    Added order creation functionality

--- express_backend/customer.js
+++ express_backend/customer.js
@@ -1,6 +1,14 @@
 import {app, pool} from 'server.js';  // Get app and database connection from server.js
 
-app.get('/customer', async (req, res) => {
+app.post('/createCustomerOrder', async (req, res) => {
+    let dateTime = new Date();
+    const statement = {
+        text: "INSERT INTO orders(date, cost) VALUES($1, 0);",
+        values: [dateTime],
+    }
+});
+
+app.post('/addCustomerOrderItem', async (req, res) => {
     // Record order item details
     var orderID = req.body.orderID;
     var priceID = req.body.priceID;
@@ -8,7 +16,6 @@ app.get('/customer', async (req, res) => {
 
     // Keep track of how many items are in this order for later sql queries
     var numItems = 1;
-
     // Check if there are more than 1 menu items in the order. If so, save them
     if (req.body.hasOwnProperty('menuItem2')) {
         var menuItem2 = req.body.menuItem2;
@@ -25,15 +32,32 @@ app.get('/customer', async (req, res) => {
     }
 
     try {  // Insert the new order into the database
+        var orderItemQuery;
         if (numItems == 1) {
-            const orderItemQuery = {
+            orderItemQuery = {
                 text: 'INSERT INTO order_item(order_item_id, order_id, price_id, menu_item1_id) VALUES((SELECT MAX(order_item_id)+1 FROM order_item), $1, $2, $3);',
                 values: [orderID, priceID, menuItem1],
             };
         }
+        else if (numItems == 2) {
+            orderItemQuery = {
+                text: 'INSERT INTO order_item(order_item_id, order_id, price_id, menu_item1_id, menu_item2_id) VALUES((SELECT MAX(order_item_id)+1 FROM order_item), $1, $2, $3, $4);',
+                values: [orderID, priceID, menuItem1, menuItem2],
+            };
+        }
+        else if (numItems == 3) {
+            orderItemQuery = {
+                text: 'INSERT INTO order_item(order_item_id, order_id, price_id, menu_item1_id, menu_item2_id, menu_item3_id) VALUES((SELECT MAX(order_item_id)+1 FROM order_item), $1, $2, $3, $4, $5);',
+                values: [orderID, priceID, menuItem1, menuItem2, menuItem3],
+            };
+        }
+        else if (numItems == 4) {
+            orderItemQuery = {
+                text: 'INSERT INTO order_item(order_item_id, order_id, price_id, menu_item1_id, menu_item2_id, menu_item3_id, menu_item4_id) VALUES((SELECT MAX(order_item_id)+1 FROM order_item), $1, $2, $3, $4, $5, $6);',
+                values: [orderID, priceID, menuItem1, menuItem2, menuItem3, menuItem4],
+            };
+        }
         const result = await pool.query(orderItemQuery);
-
-        // TODO - Handle multiple items per order
         
         const priceQuery = {  // Get the price of the newly added order item
             text: 'SELECT price FROM prices WHERE price_id = $1;',
